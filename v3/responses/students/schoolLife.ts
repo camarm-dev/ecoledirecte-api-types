d22a9e5e2e0eb0a9476cbde8d91eddd8a02cd257
camarm-dev <contact@camarm.dev>
2024-02-18 00:27:47 +0100

    [Add] schoolLife.ts types

--- v3/responses/students/schoolLife.ts
+++ v3/responses/students/schoolLife.ts
@@ -9,7 +9,38 @@ export type schoolLifeResSuccess = {
 	data: schoolLifeResData;
 };
 
+export type schoolLifeItem = {
+	id: number;
+	idEleve: number;
+	nomEleve: string;
+	typeElement: "Punition" | "Retard" | "Absence" | string;
+	date: string;
+	displayDate: string;
+	libelle: "RETENUE" | string;
+	motif: string;
+	justifie: boolean;
+	par: string;
+	commentaire: string;
+	typeJustification: string;
+	justifieEd: boolean;
+	aFaire: string;
+	dateDeroulement: string;
+}
+
 export type schoolLifeResData = {
 	// TODO
+	sanctionsEncouragements: Array<schoolLifeItem>,
+	absencesRetards: Array<schoolLifeItem>,
+	parametrage: {
+		justificationEnLigne: boolean;
+		absenceCommentaire: boolean;
+		retardCommentaire: boolean;
+		sanctionsVisible: boolean;
+		sanctionParQui: boolean;
+		sanctionCommentaire: boolean;
+		encouragementsVisible: boolean;
+		encouragementParQui: boolean;
+		encouragementCommentaire: boolean;
+	}
 };
 
