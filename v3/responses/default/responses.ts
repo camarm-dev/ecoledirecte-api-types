dd823da7f4c930794005176845d3b0291450c15c
camarm-dev <contact@camarm.dev>
2024-02-18 12:21:41 +0100

    [Fix] Global type for responses

---
+++ v3/responses/default/responses.ts
@@ -0,0 +1,19 @@
+import {loginRes} from "../login/index";
+import {cloudRes, cloudResFile, cloudResFolder} from "../global/cloud";
+import {mailboxRes, mailboxResMessage} from "../global/mailbox";
+import {timetableRes} from "../global/timetable";
+import {
+    gradesRes,
+    manualsRes,
+    schoolLifeRes,
+    studCommonTlRes,
+    studentDocsRes,
+    studTlRes,
+    textbookRes,
+    walletsRes
+} from "../students";
+import {schoolsRes, studentsRes} from "../teachers";
+
+export type response = loginRes | cloudRes | cloudResFile | cloudResFolder | mailboxRes | mailboxResMessage |
+    timetableRes | studentDocsRes | gradesRes | manualsRes | schoolLifeRes | textbookRes | studTlRes | walletsRes |
+    schoolsRes | studCommonTlRes | studentsRes
