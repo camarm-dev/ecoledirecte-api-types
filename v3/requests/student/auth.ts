1ffee6cce1d374f557b7beca14779ea326371787
camarm-dev <contact@camarm.dev>
2024-03-09 22:54:28 +0100

    fix: typed `auth.ts`; renewing token

--- v3/requests/student/auth.ts
+++ v3/requests/student/auth.ts
@@ -1,8 +1,12 @@
 import {body} from "../default/body";
+import {role} from "../../responses";
 
 export interface authRequestData extends body  {
-    identifiant: string
-    motdepasse: string
-    isRelogin: boolean
-    uuid: string
+    identifiant: string;
+    motdepasse: string;
+    uuid: string;
+    isRelogin?: boolean;
+    sesouvenirdemoi?: boolean;
+    typeCompte?: role;
+    accesstoken?: string;
 }
